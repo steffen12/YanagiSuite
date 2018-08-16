cbe7c50cd966bb18c957ddd618260af7d93c800b
mgunady <mohamedkhaled88@gmail.com>
2018-08-16 16:13:13 -0400

    running aligner cmd through python's subprocess

--- yanagiCount_simple.py
+++ yanagiCount_simple.py
@@ -3,8 +3,9 @@ from collections import Counter
 
 from Bio import SeqIO
 
-import sys, pickle
-import time
+import subprocess
+
+import os, sys, time
 
 class Seg:
     ID = ""
@@ -51,22 +52,19 @@ def load_SegmentsLib(seg_file):
 ######################################
 ######################################
 
-def saveObj(obj, name):
-    pickle.dump(obj, open(name+".p", "wb"))
-
-def loadObj(name):
-    return pickle.load(open(name+".p", "rb"))
-
 def readSAM(fin):
     readAligns = defaultdict(list)
+    #c = 0
     for line in fin:
-        if line[0] == "@": #If header line:
+        tokens = line.strip().split()
+        if line.startswith('@'): #If header line:
             continue
-        print(line)
-        tokens = line.strip().split("\t")
         readID, flags, segID, pos = tokens[:4]
         #readLen = len(tokens[9])
         readAligns[readID].append((segID, flags))
+        #c = c+1
+        #if c % 1000000 == 0:
+        #    print("Seen", str(c), "alignments")
     return(readAligns)
 
 def validOrientation(flags1, flags2):
@@ -123,61 +121,83 @@ def processPairAligns(aligns1, aligns2, segsDict):
           "Unmapped Due Txs:", readsMapped[1], "Unmapped Due Direction:", readsMapped[2])
     return(segPairs_counts, newsegPairs_counts)
 
-endIdx = int(sys.argv[1])-1
+def runAlignment(cmd):
+    print("Running Alignment Command...", cmd)
+    start_t = time.time()
+    align_proc = subprocess.Popen(cmd,
+                                  stdout=subprocess.PIPE,
+                                  stderr=sys.stdout)
+    aligns = readSAM(align_proc.stdout)
+##    buf = ""
+##    aligns = defaultdict(list)
+##    for line in align_proc.stdout:
+##        buf = buf + line
+##        if len(buf.split("\n")) > 100:
+##            print(buf.split("\n")[:-1])
+##            raw_input("Press to continue")
+##            aligns_sub = readSAM(buf.split("\n")[:-1])
+##            buf = buf[-1:]
+##            for read in aligns_sub:
+##                aligns.extend(aligns_sub[read])
+    print("Done")
+    elapsed = time.time() - start_t
+    print("Elapsed Time: ", elapsed)
+    return(aligns)
 
-fin = sys.stdin
-aligns = readSAM(fin)
-if endIdx == 0:
-    saveObj(aligns, "aligns1")
-else:
+# python yanagiCount_simple.py refs/hg37_segs_101.fa simData/output/rapmap_101/Hs_1.tsv
 
-    segmentReferenceFilename = sys.argv[2]
-    outputCountsFilename = sys.argv[3]
+segmentReferenceFilename = sys.argv[1]
+outputCountsFilename = sys.argv[2]
+
+#cmd1, cmd2 = sys.argv[3], sys.argv[4]
+cmd1 = "./rapmap quasimap -i refs/hg37_segs_101_quasiindex/ -r simData/reads/Hs_1_1.fq -t 20"
+cmd2 = "./rapmap quasimap -i refs/hg37_segs_101_quasiindex/ -r simData/reads/Hs_1_2.fq -t 20"
+
+aligns1 = runAlignment(cmd1.split())
+aligns2 = runAlignment(cmd2.split())
+
+print("Loading Segments Lib...")
+start_t = time.time()
+segsDict, segIDs = load_SegmentsLib(segmentReferenceFilename)
+print("Done!")
+elapsed = time.time() - start_t
+print("Elapsed Time: ", elapsed)
+
+print("Processing Alignments...")
+start_t = time.time()
+segPairs_counts, newsegPairs_counts = processAligns(aligns1, aligns2, segsDict)
+print("Done!")
+elapsed = time.time() - start_t
+print("Elapsed Time: ", elapsed)
+
+print("Writing Segments Counts...")
+start_t = time.time()
+with open(outputCountsFilename, "w") as f:
+    f.write("SEG1ID\tSEG2ID\tcount\tSEGTYPES\tGENE\tSEG1LEN\tSEG2LEN\tSEG1SLoc\tSEG2SLoc\tTXS\n")
     
-    aligns1 = loadObj("aligns1")
+    for segPair in sorted(segPairs_counts.iterkeys()):
+        count = segPairs_counts[segPair]
+        segs = [segs_dict[segID] for segID in segPair.split("_")]
+        types = segs[0].segtype
+        types += segs[1].segtype if segs[0].ID != segs[1].ID else ""
+        line = "\t".join([segs[0].ID, segs[1].ID, str(count), types,
+                          segs[0].geneID,
+                          str(segs[0].length), str(segs[1].length),
+                          str(segs[0].startLoc), str(segs[1].startLoc), ','.join(segPairs_txs[segPair])])
+        f.write(line + "\n")
+with open(outputCountsFilename+".newJuncs", "w") as f:
+    f.write("SEG1ID\tSEG2ID\tcount\tSEGTYPES\tSEG1GENE\tSEG2GENE\tSEG1LEN\tSEG2LEN\tSEG1SLoc\tSEG2SLoc\n")
+    
+    for segPair in sorted(newsegPairs_counts.iterkeys()):
+        count = newsegPairs_counts[segPair]
+        segs = [segs_dict[segID] for segID in segPair.split("_")]
+        types = segs[0].segtype
+        types += segs[1].segtype if segs[0].ID != segs[1].ID else ""
+        line = "\t".join([segs[0].ID, segs[1].ID, str(count), types,
+                          segs[0].geneID, segs[1].geneID,
+                          str(segs[0].length), str(segs[1].length),
+                          str(segs[0].startLoc), str(segs[1].startLoc)])
+        f.write(line + "\n")
 
-    print("Loading Segments Lib...")
-    start_t = time.time()
-    segsDict, segIDs = load_SegmentsLib(segmentReferenceFilename)
-    print("Done!")
-    elapsed = time.time() - start_t
-    print("Elapsed Time: ", elapsed)
-
-    print("Processing Alignments...")
-    start_t = time.time()
-    segPairs_counts, newsegPairs_counts = processAligns(aligns1, aligns, segsDict)
-    print("Done!")
-    elapsed = time.time() - start_t
-    print("Elapsed Time: ", elapsed)
-
-    print "Writing Segments Counts..."
-    start_t = time.time()
-    with open(outputCountsFilename, "w") as f:
-        f.write("SEG1ID\tSEG2ID\tcount\tSEGTYPES\tGENE\tSEG1LEN\tSEG2LEN\tSEG1SLoc\tSEG2SLoc\tTXS\n")
-        
-        for segPair in sorted(segPairs_counts.iterkeys()):
-            count = segPairs_counts[segPair]
-            segs = [segs_dict[segID] for segID in segPair.split("_")]
-            types = segs[0].segtype
-            types += segs[1].segtype if segs[0].ID != segs[1].ID else ""
-            line = "\t".join([segs[0].ID, segs[1].ID, str(count), types,
-                              segs[0].geneID,
-                              str(segs[0].length), str(segs[1].length),
-                              str(segs[0].startLoc), str(segs[1].startLoc), ','.join(segPairs_txs[segPair])])
-            f.write(line + "\n")
-    with open(outputCountsFilename+".newJuncs", "w") as f:
-        f.write("SEG1ID\tSEG2ID\tcount\tSEGTYPES\tSEG1GENE\tSEG2GENE\tSEG1LEN\tSEG2LEN\tSEG1SLoc\tSEG2SLoc\n")
-        
-        for segPair in sorted(newsegPairs_counts.iterkeys()):
-            count = newsegPairs_counts[segPair]
-            segs = [segs_dict[segID] for segID in segPair.split("_")]
-            types = segs[0].segtype
-            types += segs[1].segtype if segs[0].ID != segs[1].ID else ""
-            line = "\t".join([segs[0].ID, segs[1].ID, str(count), types,
-                              segs[0].geneID, segs[1].geneID,
-                              str(segs[0].length), str(segs[1].length),
-                              str(segs[0].startLoc), str(segs[1].startLoc)])
-            f.write(line + "\n")
-
-    elapsed = time.time() - start_t
-    print "Elapsed Time: ", elapsed
+elapsed = time.time() - start_t
+print("Elapsed Time: ", elapsed)
